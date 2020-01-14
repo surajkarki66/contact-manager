6af2b66303a60ff2f577621637b9fc2786da0b81
surajkarki66 <suraj.karki500@gmail.com>
2020-01-14 16:36:52 +0545

    Added sort by id

--- db/database.py
+++ db/database.py
@@ -74,6 +74,13 @@ class DB:
         contact = [{'id': row[0], 'name': row[1], 'gender':row[2], 'email':row[3], 'phone': row[4], 'Type':row[5], 'address':row[6]} for row in self.cursor.fetchall()]
         return contact
 
+    def sort_by_id(self):
+        self.cursor.execute('SELECT * FROM contact ORDER BY id')
+        self.connections.commit()
+        contact = [{'id': row[0], 'name': row[1], 'gender':row[2], 'email':row[3], 'phone': row[4], 'Type':row[5], 'address':row[6]} for row in self.cursor.fetchall()]
+        return contact
+
+
 
 
 
